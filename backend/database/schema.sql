88a4460d8174db41bc9e229d0231025cb60f97c9
Tu Nombre <tucorreo@example.com>
2026-07-30 15:07:31 -0600

    Primer commit limpio sin archivos sensibles

---
+++ backend/database/schema.sql
@@ -0,0 +1,50 @@
+CREATE TABLE usuarios (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ nombre VARCHAR(100),
+ email VARCHAR(100),
+ password VARCHAR(255),
+ rol VARCHAR(50)
+);
+
+CREATE TABLE clientes (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ nombre VARCHAR(100),
+ telefono VARCHAR(20),
+ fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE productos (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ nombre VARCHAR(150),
+ descripcion TEXT,
+ precio DECIMAL(10,2),
+ stock INT,
+ imagen VARCHAR(255),
+ activo BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE pedidos (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ numero_pedido VARCHAR(20),
+ cliente_id INT,
+ total DECIMAL(10,2),
+ estado VARCHAR(50),
+ fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE pedido_detalle (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ pedido_id INT,
+ producto_id INT,
+ cantidad INT,
+ precio_unitario DECIMAL(10,2),
+ subtotal DECIMAL(10,2)
+);
+
+CREATE TABLE historial_pedidos (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ pedido_id INT,
+ estado VARCHAR(50),
+ descripcion TEXT,
+ fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
