db06b73716cc5d4d7d0655f1c227f071dc80ead9
Tu Nombre <tucorreo@example.com>
2026-07-31 12:06:39 -0600

    Eliminar node_modules del repositorio

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,12 @@ const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const path = require('path');
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 // Servir la carpeta frontend de forma estática
 app.use(express.static(path.join(__dirname, '../frontend')));
-
 const JWT_SECRET = 'pakazita_secret_key_2026';
-
 // --- Conexión a PostgreSQL en Render ---
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -21,9 +17,7 @@ const pool = new Pool({
         rejectUnauthorized: false
     }
 });
-
 module.exports = pool;
-
 // --- Ruta base (Muestra la tienda web completa) ---
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
@@ -37,7 +31,6 @@ app.get('/api/productos', async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
-
 // --- Configuración del Puerto para Render ---
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
